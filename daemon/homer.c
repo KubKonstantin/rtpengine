c87f984de7489692a886fae826e707418f5501f3
KubKonstantin <123465354+KubKonstantin@users.noreply.github.com>
2025-07-04 17:33:08 +0600

    Update homer.c

--- daemon/homer.c
+++ daemon/homer.c
@@ -42,15 +42,6 @@ static struct homer_sender *main_homer_sender;
 
 static int send_hepv3 (GString *s, const str *id, int, const endpoint_t *src, const endpoint_t *dst,
 		const struct timeval *, int hep_capture_proto);
- if (hep_capture_proto == PROTO_RTCP && s->len >= 2) {
-        uint8_t *data = (uint8_t *)s->str;
-        uint8_t pt = data[1];  // RTCP-тип находится во втором байте
-
-        if (pt == 201) {  // Если это XR
-            data[1] = 200;  // Меняем на SR
-            ilog(LOG_DEBUG, "HEP: Replaced RTCP XR (201) with SR (200)");
-        }
- }
 // state handlers
 static int __established(struct homer_sender *hs);
 static int __in_progress(struct homer_sender *hs);
@@ -340,7 +331,13 @@ static int send_hepv3 (GString *s, const str *id, int capt_id, const endpoint_t 
     //hep_chunk_t authkey_chunk;
     hep_chunk_t correlation_chunk;
     //static int errors = 0;
-
+	if (hep_capture_proto == PROTO_RTCP && s->len >= 2) {
+        	uint8_t *data = (uint8_t *)s->str;
+        	if (data[1] == 201) {  // RTCP XR (тип во втором байте)
+            		data[1] = 200;      // Меняем на SR
+            		ilog(LOG_DEBUG, "HEP: Replaced RTCP XR (201) with SR (200)");
+        	}
+    	}
     hg = malloc(sizeof(struct hep_generic));
     memset(hg, 0, sizeof(struct hep_generic));
 
