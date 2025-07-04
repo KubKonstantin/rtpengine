22fbf6d63de7513df740b0af76403214deb35605
KubKonstantin <123465354+KubKonstantin@users.noreply.github.com>
2025-07-04 17:38:27 +0600

    Update homer.c

--- daemon/homer.c
+++ daemon/homer.c
@@ -331,13 +331,13 @@ static int send_hepv3 (GString *s, const str *id, int capt_id, const endpoint_t 
     //hep_chunk_t authkey_chunk;
     hep_chunk_t correlation_chunk;
     //static int errors = 0;
-	if (hep_capture_proto == PROTO_RTCP && s->len >= 2) {
-        	uint8_t *data = (uint8_t *)s->str;
-        	if (data[1] == 201) {  // RTCP XR (тип во втором байте)
-            		data[1] = 200;      // Меняем на SR
-            		ilog(LOG_DEBUG, "HEP: Replaced RTCP XR (201) with SR (200)");
-        	}
-    	}
+    if (hep_capture_proto == 3 && s->len >= 2) {
+        uint8_t *data = (uint8_t *)s->str;
+        if (data[1] == 201) {  // RTCP XR (тип во втором байте)
+            data[1] = 200;      // Меняем на SR
+            ilog(LOG_DEBUG, "HEP: Replaced RTCP XR (201) with SR (200)");
+        }
+    }
     hg = malloc(sizeof(struct hep_generic));
     memset(hg, 0, sizeof(struct hep_generic));
 
